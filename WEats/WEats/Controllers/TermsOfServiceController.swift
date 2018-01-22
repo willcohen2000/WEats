8696e1ada80562813163471d19cb5c0fd49486e8
willcohen2000 <willcohen2000@gmail.com>
2018-01-22 18:06:16 -0500

    Removed PDF library, added PDF functionality natively

---
+++ WEats/WEats/Controllers/TermsOfServiceController.swift
@@ -0,0 +1,29 @@
+//
+//  TermsOfServiceController.swift
+//  WEats
+//
+//  Created by Will Cohen on 1/22/18.
+//  Copyright © 2018 Will Cohen. All rights reserved.
+//
+
+import UIKit
+
+class TermsOfServiceController: UIViewController {
+
+    @IBOutlet weak var pdfWebView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        if let pdf = Bundle.main.url(forResource: "WEPrivacyPolicy", withExtension: "pdf", subdirectory: nil, localization: nil)  {
+            let req = NSURLRequest(url: pdf);
+            pdfWebView.loadRequest(req as URLRequest);
+        }
+
+    }
+
+    @IBAction func backButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil);
+    }
+    
+
+}
